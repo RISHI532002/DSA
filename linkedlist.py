ab1a2e1ad2194e408499e1052bb0a307fb4130e6
Rushikesh Patil <rushikeshpatil532002@gmail.com>
2026-02-18 23:11:07 +0530

    feat: added new methods in the linked list

--- linkedlist.py
+++ linkedlist.py
@@ -7,7 +7,7 @@ class LinkedList:
     def __init__(self):
         self.head = None
     
-    def insert_at_beginning(self, value):
+    def insert_at_start(self, value):
         new_node = Node(value)
         new_node.next = self.head
         self.head = new_node
@@ -16,33 +16,66 @@ class LinkedList:
         new_node = Node(value)
         if not self.head:
             self.head = new_node
-            return 
+            return
         temp = self.head
         while temp.next:
             temp = temp.next
         temp.next = new_node
     
-    def insert_at_position(self, index, value):
-        if index<0:
-            raise IndexError("Negative index is not allowed")
+    def insert_at_index(self, index, value):
+        """
+        zero based index
+        """
+        if index < 0:
+            raise IndexError("Index out of range")
         if index==0:
-            self.insert_at_beginning(value)
+            self.insert_at_start(value)
             return
         if not self.head:
-            raise IndexError("List index out of range")
-        
+            raise IndexError("List is empty")
+        new_node = Node(value)
         temp = self.head
-        for _ in range(index - 1):
-            if temp.next is None:
+        for _ in range(index-1):
+            if not temp.next:
                 raise IndexError("List index out of range")
             temp = temp.next
-        new_node = Node(value)
         new_node.next = temp.next
-        temp.next= new_node
-
-    def display(self):
+        temp.next = new_node
+    
+    def search_by_value(self, value):
+        if not self.head:
+            print("List is empty")
+            return False
         temp = self.head
         while temp:
-            print(temp.value)
+            if temp.value==value:
+                return True
+            temp = temp.next
+        return False
+    
+    def search_by_index(self, index):
+        if not self.head:
+            print("List is empty")
+            return None
+        if index<0:
+            raise IndexError("List index out of range")
+        if index==0:
+            return self.head.value
+        temp = self.head
+        for _ in range(index-1):
+            if not temp.next:
+                raise IndexError("List index out of range")
+            temp = temp.next
+        if not temp.next:
+            raise IndexError("List index out of range")
+        return temp.next.value
+
+    def display(self):
+        if not self.head:
+            print("None")
+            return
+        temp = self.head
+        while temp:
+            print(temp.value, end="->")
             temp = temp.next
         print("END")
