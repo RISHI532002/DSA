8812347e46adf683d1de9bd0fb457b6e2070bca2
Rushikesh Patil <rushikeshpatil532002@gmail.com>
2026-02-22 19:41:15 +0530

    Implement stack using Nodes

--- Stack.py
+++ Stack.py
@@ -1,4 +1,6 @@
-# Stack using array
+"""
+Stack using python list
+"""
 
 class Stack:
     def __init__(self, capacity):
@@ -37,3 +39,45 @@ class Stack:
         if self.isEmpty():
             return None
         return self.memory[:self.top+1]
+
+"""
+Stack using point and Nodes
+"""
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class StackLL:
+    def __init__(self):
+        self.top_node = None
+        self.size = 0
+    
+    def isEmpty(self):
+        return self.size == 0
+    
+    def push(self, value):
+        new_node = Node(value=value)
+        if self.top_node is None:
+            self.top_node = new_node
+        else:
+            new_node.next = self.top_node
+            self.top_node = new_node
+        self.size += 1
+    
+    def pop(self):
+        if self.isEmpty():
+            return None
+        temp = self.top_node
+        self.top_node = self.top_node.next
+        self.size -= 1
+        return temp.value
+    
+    def get_stack(self):
+        memory = []
+        temp = self.top_node
+        while temp:
+            memory.append(temp.value)
+            temp = temp.next
+        return memory
